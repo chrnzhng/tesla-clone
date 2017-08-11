a1ab319bdbc1bea6a4b3111271739dd428651de0
Jordan Zhang <chrn.zhng@gmail.com>
2017-08-11 11:35:55 -0600

    Styled buttons, corrected flexing in header

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -7,30 +7,35 @@ class LandingPage extends Component {
 
             <div className="background">
                 <div className="header">
-                    <img className="logo" src='https://s3-us-west-1.amazonaws.com/tesla-clone/tesla-landing/tsla-logo.svg' />
-                    <ul className="header-nav-lnk">
-                        <li>MODEL S</li>
-                        <li>MODEL X</li>
-                        <li>MODEL 3</li>
-                        <li>ENERGY </li>
-                        
-                    </ul>
+                    <div className="header-container">
+                        <img
+                            className="logo"
+                            src='https://s3-us-west-1.amazonaws.com/tesla-clone/tesla-landing/tsla-logo.svg'/>
+                        <ul className="header-nav-lnk">
+                            <li>MODEL S</li>
+                            <li>MODEL X</li>
+                            <li>MODEL 3</li>
+                            <li>ENERGY
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
                 <video src={require('./landing-vid.mp4')} autoPlay loop></video>
                 <div className="intro-container">
-                <div className="wrapper-intro">
-                    <h1>Quickest Acceleration.<br></br>
-                        Longest Range. The Safest Cars Ever.</h1>
-                <div className="group-btn">
-                    <button>ORDER YOURS</button>
-                    <button>NEW INVENTORY</button>
-                    <button>USED INVENTORY</button>
-                    <button>TEST DRIVE</button>
-                </div>
-                <div className="newsletter">
-                    <h5>GET THE NEWSLETTER</h5>
-                </div>
-                </div>
+                    <div className="wrapper-intro">
+                        <h1>Quickest Acceleration.<br></br>
+                            Longest Range. The Safest Cars Ever.</h1>
+                        <div className="group-btn">
+                            <button className="trans-btn"><p>ORDER YOURS</p></button>
+                            <button className="trans-btn"><p>NEW INVENTORY</p></button>
+                            <button className="trans-btn"><p>USED INVENTORY</p></button>
+                            <button className="trans-btn"><p>TEST DRIVE</p></button>
+                        </div>
+                        <div className="newsletter">
+                            <h5>GET THE NEWSLETTER</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
 
